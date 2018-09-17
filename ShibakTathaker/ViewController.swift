e459a366dc5a11fc876afb98c235b3083ea7e0e7
Muneera <muneeraaldaoud@gmail.com>
2018-09-17 21:46:17 +0300

    Initial Commit

---
+++ ShibakTathaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShibakTathaker
+//
+//  Created by Muneera AlDaoud on 07/01/1440 AH.
+//  Copyright © 1440 Muneera AlDaoud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
